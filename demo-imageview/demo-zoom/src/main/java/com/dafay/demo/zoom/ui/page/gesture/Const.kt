3e962fdeedbc5e83ffedf75d1486065b5d9aabe5
李 mac m1 studio <m1studio@lideMac-Studio.local>
2024-05-20 00:06:55 +0800

    事件冲突的问题

--- demo-imageview/demo-zoom/src/main/java/com/dafay/demo/zoom/ui/page/gesture/Const.kt
+++ demo-imageview/demo-zoom/src/main/java/com/dafay/demo/zoom/ui/page/gesture/Const.kt
@@ -4,6 +4,14 @@ package com.dafay.demo.zoom.ui.page.gesture
 
 
 
-val DEFAULT_MAX_ZOOM = 5f
+val DEFAULT_MAX_ZOOM = 3f
+val DEFAULT_MID_ZOOM = 1.75f
 val DEFAULT_MIN_ZOOM = 1f
 val DEFAULT_ANIM_DURATION = 400L
+
+enum class Edge(val value: Int){
+    EDGE_NONE(-1),
+    EDGE_LEFT(0),
+    EDGE_RIGHT(1),
+    EDGE_BOTH(2)
+}
